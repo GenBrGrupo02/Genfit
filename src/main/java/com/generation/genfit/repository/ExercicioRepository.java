98b93730d322a1644a632bbb452bbddc5e106320
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2025-01-29 12:26:35 -0300

    deploy

--- src/main/java/com/generation/genfit/repository/ExercicioRepository.java
+++ src/main/java/com/generation/genfit/repository/ExercicioRepository.java
@@ -1,9 +1,11 @@
 package com.generation.genfit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.genfit.model.Exercicio;
 
+@Repository
 public interface ExercicioRepository extends JpaRepository<Exercicio, Long>{
 	
 
