24d4ddb4897bd6b0d475e4a495c268a2132b4c32
StPVieira <pedrohq.vieira168@gmail.com>
2025-01-30 11:08:19 -0300

    fix: added origin

--- src/main/java/com/generation/genfit/controller/UsuarioController.java
+++ src/main/java/com/generation/genfit/controller/UsuarioController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = "", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
     @Autowired
